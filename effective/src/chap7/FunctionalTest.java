1715fd1f236c61203578eac3c3a9b35ea8664fdb
moon_ <m3252@naver.com>
2020-07-26 15:32:04 +0900

    first commit

---
+++ effective/src/chap7/FunctionalTest.java
@@ -0,0 +1,11 @@
+package chap7;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
+public class FunctionalTest {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ effective/src/chap7/StreamCharTest.java
@@ -0,0 +1,12 @@
+package chap7;
+
+import java.util.Scanner;
+
+public class StreamCharTest {
+
+    public static void main(String[] args) {
+        "Hello world!".chars().forEach(System.out::print);
+        "Hello world!".chars().forEach(x -> System.out.println((char) x));
+    }
+    Scanner
+}
