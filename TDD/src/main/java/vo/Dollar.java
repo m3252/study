819d5e8c9a8f1d0fcf920d9baae20941ff9b95b5
moon_ <m3252@naver.com>
2020-09-06 16:23:33 +0900

    병합

---
+++ TDD/src/main/java/vo/Dollar.java
@@ -0,0 +1,14 @@
+package vo;
+
+public class Dollar extends Money{
+
+    public Dollar(int amount, String currency){
+        super(amount, currency);
+    }
+
+    @Override
+    public String currency() {
+        return currency;
+    }
+
+}
---
+++ TDD/src/main/java/vo/Franc.java
@@ -0,0 +1,14 @@
+package vo;
+
+public class Franc extends Money{
+
+    public Franc(int amount, String currency){
+        super(amount, currency);
+    }
+
+    @Override
+    public String currency() {
+        return currency;
+    }
+
+}
