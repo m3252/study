f045a3bd5bdcd593ee361fbc3c4b1b493de5370c
moon_ <m3252@naver.com>
2020-09-14 01:15:06 +0900

    계산기

---
+++ tdd/src/main/java/calculator/Calculator.java
@@ -0,0 +1,43 @@
+package calculator;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Calculator {
+
+    private int value = 0;
+    private List<Integer> historyList = new ArrayList<>();
+
+    Calculator(){ }
+    Calculator(int value){
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
+
+    public int add(int input){
+        this.value += input;
+        addHistory(getValue());
+        return getValue();
+    }
+
+    private void addHistory(int result){
+        historyList.add(result);
+    }
+
+    public int getHistory(int index){
+        if(0 > index || index > historyList.size() -1){
+            return -1;
+        }
+        return Optional.ofNullable(historyList.get(index)).orElse(-1);
+    }
+
+    public void clear(){
+        historyList.clear();
+    }
+
+
+}
---
+++ tdd/src/main/java/calculator/CalculatorTest.java
@@ -0,0 +1,62 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeEach
+    public void 초기화(){
+        calc = new Calculator(20);
+    }
+
+
+    @Test
+    public void 계산기_초기값은_0이다(){
+        Calculator calc = new Calculator();
+        assertEquals(0, calc.getValue());
+    }
+
+    @Test
+    public void 생성자를_통해서_값을_주입한다(){
+        Calculator calc1 = new Calculator(20);
+        Calculator calc2 = new Calculator(30);
+        assertFalse(calc2.getValue() == 20);
+        assertTrue(calc2.getValue() == 30);
+        assertTrue(calc1.getValue() == 20);
+     }
+
+    @Test
+    public void 더하기_함수를_제공한다(){
+        Calculator calc = new Calculator(20);
+        assertEquals(20, calc.getValue());
+        calc.add(10);
+        assertEquals(30, calc.getValue());
+    }
+
+    @Test
+    public void 히스토리를_쌓는다(){
+        Calculator calc = new Calculator(20);
+        assertEquals(20, calc.getValue());
+        calc.add(10);
+        assertEquals(calc.getHistory(0), 30);
+        calc.add(10);
+        assertEquals(calc.getHistory(1), 40);
+        calc.add(10);
+        assertEquals(calc.getHistory(2), 50);
+    }
+
+    @Test
+    public void 히스토리를_지운다(){
+        calc.add(10);
+        assertTrue(calc.getHistory(0) == 30 );
+        calc.clear();
+        assertTrue(calc.getHistory(0) == -1);
+
+    }
+}
