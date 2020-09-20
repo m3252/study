2fda809f7f91224bd24644330f09eb9b23150e15
moon_ <m3252@naver.com>
2020-09-20 12:14:29 +0900

    3. 컴포즈 패턴 메소드 적용. (1단계 추상화)

--- tdd/src/main/java/string_calc/StringCalculator.java
+++ tdd/src/main/java/string_calc/StringCalculator.java
@@ -4,11 +4,20 @@ public class StringCalculator {
 
     // 1. 들여쓰기(2depth)를 제거하고 else를 제거
     // 2. 메소드에서 한 가지 일만 하도록 변경, 로컬 변수 제거
+    // 3. 컴포즈 패턴 메소드 적용. (1단계 추상화)
     public static int splitAndSum(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
         }
-        return sum(toInts(text.split(",|:")));
+        return sum(toInts(split(text)));
+    }
+
+    private static boolean isBlank(String text){
+        return text == null || text.isEmpty();
+    }
+
+    private static String[] split(String text){
+        return text.split(",|:");
     }
 
     private static int[] toInts(String[] values) {
