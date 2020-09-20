a272c2502b1634519de870062bf6036d05350f68
moon_ <m3252@naver.com>
2020-09-20 12:03:23 +0900

    들여쓰기(2)를 제거하고 else를 제거한 버전

--- tdd/src/main/java/string_calc/StringCalculator.java
+++ tdd/src/main/java/string_calc/StringCalculator.java
@@ -2,15 +2,19 @@ package string_calc;
 
 public class StringCalculator {
 
+    // 2 들여쓰기를 제거하고 else를 제거한 버전
     public static int splitAndSum(String text){
-        int result = 0;
         if(text == null ||text.isEmpty()){
-            result = 0;
-        } else {
-            String[] values = text.split(",|:");
-            for(String value : values){
-                result += Integer.parseInt(value);
-            }
+            return 0;
+        }
+        String[] values = text.split(",|:");
+        return sum(values);
+    }
+
+    public static int sum(String[] values){
+        int result = 0;
+        for(String value : values){
+            result += Integer.parseInt(value);
         }
         return result;
     }
