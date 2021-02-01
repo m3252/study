517ce525b8642df21cf3ea1eb42811887ca56542
moonseungchan4917 <moon.unimewo@gmail.com>
2021-02-01 12:39:53 +0900

    feat : 오브젝트 예제

---
+++ object/src/main/java/e01/Ticket.java
@@ -0,0 +1,10 @@
+package e01;
+
+public class Ticket {
+
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
---
+++ object/src/main/java/e01/TicketSeller.java
@@ -0,0 +1,4 @@
+package e01;
+
+public class TicketSeller {
+}
